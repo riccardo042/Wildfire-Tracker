003b2bc84f7931e20ee59e61f82cc84e73799a5c
riccardo <riccardoverace12@gmail.com>
2025-02-22 01:26:16 +0100

    Responsive queries

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,7 +1,7 @@
 
 const LocationInfobox = ({ info, onClick }) => {
   return (
-    <div id="infobox" className="rounded-lg opacity-80 text-neutral-100 text-md w-[20vw] h-[17vh] bg-zinc-800 absolute top-[80%] left-4 p-2">
+    <div id="infobox" className="rounded-lg opacity-80 text-neutral-100 text-md w-[20vw] h-auto bg-zinc-800 absolute top-[80%] left-4 p-2">
         <h2>Event Location Info</h2>
         <button onClick={onClick} className="absolute bottom-[80%] right-4"><img src="/mdi--remove.svg" alt="Close Icon"/></button>
         <ul>
